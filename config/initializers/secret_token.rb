9a795780e794c86284a7d283390b6a99f0c94ba0
lackita <lackita+github@gmail.com>
2011-08-02 08:09:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChuckAndElmo::Application.config.secret_token = 'ebb1dedc247d981f3835b3cd999c93d20fe453b32711793d999d64e39cffc8b790815f9a64799a26e4ea96139776a85e730da0b08cb0265a669f01ac1804ed41'
